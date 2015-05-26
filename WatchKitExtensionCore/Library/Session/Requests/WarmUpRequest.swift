a8abd0860097d60a9490846aae8edad8a7602736
Thomas Visser <thomas.visser@gmail.com>
2015-05-26 11:32:40 +0200

    Made WarmUpRequest public

--- WatchKitExtensionCore/Library/Session/Requests/WarmUpRequest.swift
+++ WatchKitExtensionCore/Library/Session/Requests/WarmUpRequest.swift
@@ -13,16 +13,16 @@ import Shared
 import Result
 
 // Returns the local time of the phone, which is just a (lame) excuse to wake up the main app
-struct WarmUpRequest: ParentAppRequest, Serializable {
+public struct WarmUpRequest: ParentAppRequest, Serializable {
     typealias ResponseType = NSDate
     
-    let identifier = HSWatchKitWarmUpRequestIdentifier
+    public let identifier = HSWatchKitWarmUpRequestIdentifier
     
-    func jsonRepresentation() -> JSON {
+    public func jsonRepresentation() -> JSON {
         return JSON([:])
     }
     
-    let responseDeserializer = { (json:JSON) in
+    public let responseDeserializer = { (json:JSON) in
         return json.int.map { int in
             return Result(value: (NSDate(timeIntervalSince1970: NSTimeInterval(int))))
         } ?? Result(error: Error.DeserializationFailed(object: json))
